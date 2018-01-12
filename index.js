33a9b03f94a341ff7efdae97b933c50355a05047
chap <chaplonglau@gmail.com>
2018-01-12 14:07:27 -0800

    challenge 2

--- index.js
+++ index.js
@@ -7,33 +7,30 @@ $(document).ready(function(){
 
 all_data=[]
 
-function getData(url,data_file){
-  $.getJSON(url,function(data){
-    debugger
-    data_file.concat(data.menus) 
-    console.log(data_file)
-    return data_file
-  })
-}
+// function getData(url,data_file){
+//   $.getJSON(url,function(data){
+//     data_file.concat(data.menus) 
+//     console.log(data_file)
+//     return data_file
+//   })
+// }
+
+// function pullData(){
+//   url="https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=1"
+//   console.log(url)
+//   $.getJSON(url,function(data){
+//     all_data=data.menus
+//     while (data.pagination.current_page * data.pagination.per_page < data.pagination.total){
+//       url=url.substring(0,url.length-1)+`${data.pagination.current_page+1}`
+//       all_data=getData(url,all_data)
+//     }
+//   })
+// }
+
 
 function pullData(){
-  url="https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=1"
-  console.log(url)
+  url="https://raw.githubusercontent.com/chaplonglau/shopify_challenge/master/challenge.json"
   $.getJSON(url,function(data){
-    all_data=data.menus
-    debugger
-    while (data.pagination.current_page * data.pagination.per_page < data.pagination.total){
-      url=url.substring(0,url.length-1)+`${data.pagination.current_page+1}`
-      all_data=getData(url,all_data)
-    }
-  })
-}
-
-
-function dotdata(){
-  url="https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=1"
-  $.getJSON(url,function(data){
-
     answer = 
       {
       "valid_menus": [
@@ -44,35 +41,47 @@ function dotdata(){
       ]
     }
 
-    var mySet= new Set()
+    var visitedSet= new Set()
     for (var menu in data.menus){
-      console.log(menu)
-      if ('parent_id' in data.menus[menu]){
-        //this is a child node. skip
+      if ('parent_id' in data.menus[menu] || visitedSet.has(data.menus[menu].id)){
+        //this is a child node or we visited it before skip.
         continue;
       }
       else {
         //this is a parent node
-        dps(mySet,menu,data)
+        var mySet= new Set()
+        visitedSet=dps(mySet,menu,data,answer)
       }
     }
-
+    json=JSON.stringify(answer)
+    debugger
   })
 }
 
 
-function dps(set,current_menu,data){
-  if (set.has(current_menu.id)){
+function dps(set,current_menu,data,answer){
+  if (set.has(data.menus[current_menu].id)){
     //this is a cycle
+    answer["invalid_menus"].push({"root_id": [...set][0], "children": [...set]})
+    return set
   }
   else {
      set.add(data.menus[current_menu].id)
-     if (data.menus[current_menu].child_ids.length !== 0){
-        dps(set,data.menus[current_menu].child_ids[0]-1,data)
+     if (data.menus[current_menu].child_ids.length === 1 ){
+        dps(set,data.menus[current_menu].child_ids[0]-1,data,answer)
+     }
+     else if (data.menus[current_menu].child_ids.length >= 1 ) {
+      data.menus[current_menu].child_ids.forEach((child,index)=>{
+         dps(set,data.menus[current_menu].child_ids[index]-1,data,answer)
+      })
      }
      else {
         //this is not a cycle
-       answer["valid_menus"].push({"root_id": [...set][0], "children": [...set].shift()})
+       var set2=[...set]
+       set2.shift()
+       answer["valid_menus"].push({"root_id": [...set][0], "children": [...set2]})
+       debugger
      }
+     return set
   }
 }
